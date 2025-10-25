ac42dabdb50a7213ee761c989f9e4416d0e30893
Bamsen <81903806+bamsen04@users.noreply.github.com>
2025-10-25 18:04:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miracle-db",
+    version="1.0.0",
+    author="Bamsen",
+    author_email="bamsen@gaupestudio.com",
+    description="Faith-based database engine. Just pray for your data.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
